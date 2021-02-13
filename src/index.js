ec3a666ede1a92c84ad2811c197b1da06c8ae85a
Romdra <roman_dra@mail.ru>
2021-02-13 19:00:47 +0500

    fix: fixed task

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 
 exports.min = function min(array) {
-  return Math.min.apply(null, array)
-}
+  if (array && array.length) {
+    return Math.min.apply(null, array)
+  } else {
+    return 0
+  }
+} 
+
 
 exports.max = function max(array) {
-  return Math.max.apply(null, array)
+  if (array && array.length) {
+    return Math.max.apply(null, array)
+  }else {
+    return 0
+  }
 }
 
+
 exports.avg = function avg(array) {
-  sum = 0;
-  for (let i = 0; i < array.length; i++)
-  sum += array[i]
-  return sum/array.length;
-}
+  if (array && array.length) {
+    sum = 0;
+    for (let i = 0; i < array.length; i++)
+    sum += array[i]
+    return sum/array.length;
+  } else {
+    return 0
+  }
+} 
